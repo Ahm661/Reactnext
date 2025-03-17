60e508ba3625a2521f2470827bfe4432cbd455ce
Your Name <you@example.com>
2025-03-17 09:20:21 +0100

    Video 3 Finished

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -4,23 +4,47 @@ import { Car } from '@lib/types/types'
 
 type Props = {}
 
-const car:Car = {
-    name: "Super Car",
-    type: "BMW M3",
-    color: "red",
-    motor: {
-        serialNumber: 1234,
-        hp: 440,
+const cars: Car[] = [
+    {
+        name: "Super Car",
+        type: "BMW M3",
+        color: "red",
+        motor: {
+            serialNumber: 1234,
+            hp: 440,
+        }
+    },
+
+    {
+        name: "Mega Car",
+        type: "Mercedes G-Class",
+        color: "Black",
+        motor: {
+            serialNumber: 5678,
+            hp: 670,
+        }
+    },
+
+    {
+        name: "Ultra car",
+        type: "Porsche 911",
+        color: "White",
+        motor: {
+            serialNumber: 7891,
+            hp: 820,
+        }
     }
-}
+]
+
 
 export default function Cars({ }: Props) {
     return (
         <div>
             <h1>Cars</h1>
-            <Card car={car} />
-            <Card car={car} />
-            <Card car={car} />
+            {cars.map((car, index) => (
+            <Card key={index} car={car} />
+            ))}
+
         </div>
-    )
+    );
 }
