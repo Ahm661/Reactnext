e4a74efb4b95bf668e11fcca51361d84db4d08ae
Your Name <you@example.com>
2025-03-17 09:04:43 +0100

    Video 2 finished

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Card from "../components/car/card"
+import { Car } from '@lib/types/types'
 
 type Props = {}
 
-export default function Cars({}: Props) {
-  return (
-    <div>Cars</div>
-  )
+const car:Car = {
+    name: "Super Car",
+    type: "BMW M3",
+    color: "red",
+    motor: {
+        serialNumber: 1234,
+        hp: 440,
+    }
+}
+
+export default function Cars({ }: Props) {
+    return (
+        <div>
+            <h1>Cars</h1>
+            <Card car={car} />
+            <Card car={car} />
+            <Card car={car} />
+        </div>
+    )
 }
